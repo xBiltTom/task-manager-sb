70a875df6345d757c02c5d9fe9b64addc611c336
xBiltTom <biltonjr@hotmail.com>
2026-02-25 09:14:35 -0500

    First commit

---
+++ src/test/java/com/xdevelopers/spring_todo/SpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdevelopers.spring_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
